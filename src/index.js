293a30c52720e357f4c6ae8c929e991f1a45a31f
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-29 20:48:28 +0300

    handle deepgram message with utterance end event

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { createDeepgramConnection } from "./deepgram.js";
 import { createElevenLabsConnection } from "./elevenlabs.js";
-import { chat } from "./openai.js";
+import { chat } from "./groq.js";
 import { getLLMCost, getTTSCost, getTranscriptionCost } from "./pricing.js";
 import prettyms from "pretty-ms";
 import fs from "fs";
 import url from "url";
 
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 
-const deepgramMessages = [];
 const chatHistory = [];
+const deepgramMessages = [];
 const inputAudioChunks = [];
 const outputAudioChunks = [];
 
@@ -23,100 +23,101 @@ wss.on("connection", (ws, req) => {
   const startTimestamp = Date.now();
 
   const deepgram = createDeepgramConnection();
-
-  const elevenlabs = createElevenLabsConnection({
-    output_format: output_format ?? "pcm_16000",
-  });
-
   const isDeepgramOpen = () => deepgram.readyState === WebSocket.OPEN;
-  const isElevenLabsOpen = () => elevenlabs.readyState === WebSocket.OPEN;
+
+  const currentTranscripts = [];
+
+  // const elevenlabs = createElevenLabsConnection({
+  //   output_format: output_format ?? "pcm_16000",
+  // });
+  // const isElevenLabsOpen = () => elevenlabs.readyState === WebSocket.OPEN;
 
   deepgram.addEventListener("message", async (messageEvent) => {
     const data = JSON.parse(messageEvent.data);
 
     deepgramMessages.push(data);
 
-    if (data.type !== "Results") {
-      return;
+    if (data.type === "Results") {
+      const transcript = data.channel.alternatives[0].transcript;
+
+      if (!transcript) {
+        return; // we don't care about empty transcripts
+      }
+
+      if (!data.is_final) {
+        return; // we don't care about non-final results
+      }
+
+      console.log("[DEEPGRAM 🎥] T:", transcript);
+
+      currentTranscripts.push(transcript);
     }
 
-    if (!data.is_final) {
-      return; // we don't care about interim results
+    if (data.type === "UtteranceEnd") {
+      const prompt = currentTranscripts.join(" ");
+
+      // console.log("[LLM] prompt:", prompt);
+      console.time("llm response time");
+      const message = await chat(prompt, chatHistory);
+      console.timeEnd("llm response time");
+      console.log("[LLM] ", message.content);
+
+      chatHistory.push({ prompt, message });
     }
 
-    const transcript = data.channel.alternatives[0].transcript;
+    // if (!isElevenLabsOpen()) {
+    //   return; // we can't send anything to elevenlabs if we're not connected yet
+    // }
 
-    if (!transcript) {
-      return; // we don't care about empty transcripts
-    }
-
-    if (!isElevenLabsOpen()) {
-      return; // we can't send anything to elevenlabs if we're not connected yet
-    }
-
-    console.log("[DEEPGRAM 🎥] T:", transcript);
-
-    const message = await chat(transcript, chatHistory);
-
-    chatHistory.push({
-      prompt: transcript,
-      message,
-    });
-
-    console.log("[OPENAI] LLM response:", message.content);
-
-    // console.log("[ELVENLABS] sending text", resp);
-    elevenlabs.send(
-      JSON.stringify({
-        text: message.content + " ",
-        flush: true,
-      }),
-    );
+    // elevenlabs.send(
+    //   JSON.stringify({
+    //     text: message.content + " ",
+    //     flush: true,
+    //   }),
+    // );
   });
 
-  elevenlabs.addEventListener("message", (messageEvent) => {
-    const data = JSON.parse(messageEvent.data);
+  // elevenlabs.addEventListener("message", (messageEvent) => {
+  //   const data = JSON.parse(messageEvent.data);
 
-    if (data.audio) {
-      console.log(
-        "[ELEVENLABS] Recieved audio chunk:",
-        data.audio.slice(0, 15) + "...",
-      );
+  //   if (data.audio) {
+  //     console.log(
+  //       "[ELEVENLABS] Recieved audio chunk:",
+  //       data.audio.slice(0, 15) + "..."
+  //     );
 
-      const buf = Buffer.from(data.audio, "base64");
+  //     const buf = Buffer.from(data.audio, "base64");
 
-      outputAudioChunks.push(buf);
+  //     outputAudioChunks.push(buf);
 
-      ws.send(buf);
-    }
-  });
+  //     ws.send(buf);
+  //   }
+  // });
 
   ws.on("message", (message) => {
     inputAudioChunks.push(message);
 
-    if (isDeepgramOpen()) {
-      deepgram.send(message);
+    if (!isDeepgramOpen()) {
+      return;
     }
+
+    deepgram.send(message);
   });
 
-  // ws.on("pong", heartbeat);
   ws.on("close", () => {
     deepgram.close();
+    // elevenlabs.close();
 
     const endTimestamp = Date.now();
     const duration = endTimestamp - startTimestamp;
 
     console.log("[DEEPGRAM 🎥] Duration:", prettyms(duration));
 
-    elevenlabs.close();
-
     const tmpFolder = `./tmp/${startTimestamp}`;
 
     if (!fs.existsSync(tmpFolder)) {
       fs.mkdirSync(tmpFolder);
     }
-
-    // TODO: save audio chunks
 
     fs.writeFileSync(
       `${tmpFolder}/output.mp3`,
